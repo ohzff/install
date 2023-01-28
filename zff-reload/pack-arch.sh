69e504026a5ab4120d9f24992b88c97d3401c358
langonginc <langonginc@yeah.net>
2023-01-28 20:43:14 +0800

    test arch

---
+++ zff-reload/pack-arch.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# clone to usr/share/zff_reload
+git clone https://github.com/ohzff/Zff-Reload ./tmp
+
+# gather information
+cd ./tmp
+version="$(git describe --tag)"
+cd ../
+
+# download source
+curl -fsSL https://github.com/ohzff/Zff-Reload/archive/refs/tags/$version.tar.gz
+
+cat <<EOX > "./archlinux/PKGBUILD"
+pkgname='zff-reload'
+pkgver='$version'
+pkgrel=1
+pkgdesc="New Zff, the lightest adventure game."
+arch=('any')
+url='https://github.com/ohzff/Zff-Reload'
+license=('MIT')
+groups=()
+depends=()
+makedepends=('gcc')
+checkdepends=()
+optdepends=()
+provides=()
+conflicts=()
+replaces=()
+backup=()
+options=()
+install=
+changelog=
+source=('$version.tar.gz')
+noextract=()
+sha256sums=('SKIP')
+
+build(){
+    cd "$version"
+    rm ./lib/system/data/DATA.hpp
+    cp ./lib/system/data/DATA_usr.hpp ./lib/system/data/DATA.hpp
+    g++ ./main.cpp -o zff -std=c++11 -pthread
+}
+
+package(){
+    cd "$version"
+    cp "./zff" "$pkgdir/usr/bin/zff"
+    mkdir -p "$pkgdir/usr/share/zff_reload"
+    cp -r "./usr" "$pkgdir/usr/share/zff_reload/"
+    cp "./LICENCE" "$pkgdir/usr/share/zff_reload/"
+    cp "./README.md" "$pkgdir/usr/share/zff_reload/"
+}
+EOX
+
+# backup: delete first '/'
